3f8c81d726bcf61bf04d51e106f18ca469603c55
Necromoshka <sergeyklimets@gmail.com>
2016-04-13 02:05:49 +0300

    refactor

--- gui/main.py
+++ gui/main.py
@@ -5,38 +5,37 @@ from xls2db import xls2db
 
 root = Tk()
 fnm = ''
-def Quit(ev):
+
+
+def quit_f():
     global root
     root.destroy()
 
-def LoadFile(ev):
+
+def loadfile():
     global fnm
-    fn = filedialog.Open(root, filetypes = [('*.xls XLS Files', '.xls')]).show()
+    fn = filedialog.Open(root, filetypes=[('*.xls XLS Files', '.xls')]).show()
     if fn == '':
         return
     fnm = fn
 
-def SaveFile(ev):
+
+def savefile():
     global fnm
-    fn = filedialog.SaveAs(root, filetypes = [('*.db sqlite3 files', '.db')]).show()
+    fn = filedialog.SaveAs(root, filetypes=[('*.db sqlite3 files', '.db')]).show()
     if fn == '':
         return
     if not fn.endswith(".db"):
         fn += ".db"
+    xls2db(fnm, fn)
 
-
-    xls2db(fnm,fn)
-
-loadBtn = ttk.Button(root, text = 'XLS Load')
-saveBtn = ttk.Button(root, text = 'DB Save')
-quitBtn = ttk.Button(root, text = 'Quit')
-
-loadBtn.bind("<Button-1>", LoadFile)
-saveBtn.bind("<Button-1>", SaveFile)
-quitBtn.bind("<Button-1>", Quit)
-
+loadBtn = ttk.Button(root, text='XLS Load')
+saveBtn = ttk.Button(root, text='DB Save')
+quitBtn = ttk.Button(root, text='quit_f')
+loadBtn.bind("<Button-1>", loadfile)
+saveBtn.bind("<Button-1>", savefile)
+quitBtn.bind("<Button-1>", quit_f)
 loadBtn.pack()
 saveBtn.pack()
 quitBtn.pack()
-
 root.mainloop()
